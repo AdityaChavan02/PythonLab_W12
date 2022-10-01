c73451cab33a478c1e380dafcfae525441a4d533
Aditya <chavanadityawork@gmail.com>
2022-10-01 13:33:13 +0100

    Trial Update 1

--- W1_code1.py
+++ W1_code1.py
@@ -1,7 +1,8 @@
-def print_names_of_people(a):
+def print_names_of_people(a,b):
     for i in range(len(a)):
-        print("Name", i+1,"is",":",a[i])
+        print("Full Name", i+1,"is",":",a[i], b[i])
 
 names = ["Aditya","Tanvi","Maverick","Amod"]
+surname = ["Chavan","Junghare","Chavan","Chavan"]
+print_names_of_people(names,surname)
 
-print_names_of_people(names)
